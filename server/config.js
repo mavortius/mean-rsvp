f71c9f4b9b8c073acf7baf5c39d77571220025ab
Marcelo Martins <m.a.martins@live.com>
2018-03-10 23:10:40 -0300

    Part 1

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'mavortius.auth0.com',
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://admin:admin@ds012678.mlab.com:12678/mean'
+};
